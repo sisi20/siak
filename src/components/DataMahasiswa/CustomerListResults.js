43cc767764b7b5e8a525c096ee6c089eecf3a511
sisi20 <53111167+sisi20@users.noreply.github.com>
2021-12-10 16:44:27 +0700

    Update CustomerListResults.js

--- src/components/DataMahasiswa/CustomerListResults.js
+++ src/components/DataMahasiswa/CustomerListResults.js
@@ -146,7 +146,7 @@ function CustomerListResults() {
               <Stack sx={{ width: '100%' }} spacing={2}>
                 <Alert severity="info">
                   <AlertTitle>Info</AlertTitle>
-                  Tabel Ruangan — <strong> KOSONG</strong>
+                  Tabel Mahasiswa — <strong> KOSONG</strong>
                 </Alert>
               </Stack>
             </Card>
